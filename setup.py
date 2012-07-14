57f2f518918a324efe58a51252448bc0d72eb129
John O'Connor <tehjcon@gmail.com>
2012-07-14 12:59:33 -0400

    Add python setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='python-which',
+      version='0.1',
+      description='Python implementation of GNU-which command',
+      author='John O\'Connor',
+      url='https://github.com/jc0n/python-which',
+      py_modules=['which']
+)
