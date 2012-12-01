eb5d506abc74e1f2b77a2f8822e2c5f1cf23c680
John O'Connor <tehjcon@gmail.com>
2012-11-30 21:47:42 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@
 
 from distutils.core import setup
 
+import which
+
 setup(name='python-which',
-      version='0.1',
+      version=which.__version__,
+      author=which.__author__,
       description='Python implementation of GNU-which command',
-      author='John O\'Connor',
       url='https://github.com/jc0n/python-which',
       py_modules=['which']
 )
